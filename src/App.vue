e59abab64254a1cf3fec929ca2f758d46d6eb001
Ed Prykhozhay <true.a.e.p.o@gmail.com>
2020-10-14 11:42:37 +0300

    Install webpack scss pug vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div#header
+    div#adaptive
+      div#header__logo Edward Prykhozhay
+      Menu
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default class App extends Vue {
+  
+}
+</script>
+
+<style lang="scss">
+  @import "@/stylesheets/main.scss";
+</style>
